912ad757476a6217098997e8f06a33f9d1a8429f
Kartik Patel <kartik.patel@openlane.com>
2019-12-20 19:45:37 -0500

    listen to service callback and update UI with git histroy data

--- src/app/components/history/history.component.ts
+++ src/app/components/history/history.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GithubService } from 'src/app/services/github.service';
 
 @Component({
   selector: 'app-history',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
+  private git = {
+    authToken: '',
+    userName: '',
+    repo: ''
+  };
+  private commits = [];
 
-  constructor() { }
+  constructor(private gitHubService: GithubService) { }
 
   ngOnInit() {
   }
-
+  getHistory() {
+    this.gitHubService.getHistory(this.git.userName, this.git.repo, this.git.authToken);
+    this.gitHubService.eventCallback$.subscribe(data => {
+      this.showHistory(data);
+    });
+  }
+  showHistory(data) {
+    this.commits = data.map(element => {
+      return {
+        hash: element.sha,
+        link: element.html_url,
+        date: new Date(element.commit.committer.date).toDateString(),
+        message: element.commit.message,
+        committer: element.commit.committer.name
+      };
+    });
+  }
 }
